92f6d3c222f861bd1c4d5e4f021a6ccf2fd07fc9
theCapm <rmattgrimsley@gmail.com>
2021-04-28 22:44:47 -0500

    Completed Course Project - The Basics

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Lasagna',
+      'This is a lasagna recipe',
+      'https://www.thewholesomedish.com/wp-content/uploads/2018/07/Best-Lasagna-3.jpg'
+    ),
+    new Recipe(
+      'Cake',
+      'This is a cake recipe',
+      'https://i.ytimg.com/vi/qtlhdIfojmc/maxresdefault.jpg'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
