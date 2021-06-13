f04c908d1841c26a55c19919b82795234eef0991
theCapm <rmattgrimsley@gmail.com>
2021-06-12 22:35:21 -0500

    finished http section with issues
    Fetch Recipes isn't working currently due to an issue with the code the course described

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
@@ -20,14 +21,22 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http
+    debugger;
+    return this.http
       .get<Recipe[]>(
         'https://angular-course-project-54241-default-rtdb.firebaseio.com/recipes.json'
+      ).pipe(
+        map((recipes) => {
+          return recipes.map((recipe) => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        }),
+        tap(recipes => {
+          this.recipeService.setRecipes(recipes);
+        })
       )
-      .subscribe((recipes) => {
-        debugger;
-        console.log(recipes);
-        this.recipeService.setRecipes(recipes);
-      });
   }
 }
